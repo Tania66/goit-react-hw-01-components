7e60c6e86833c02bb471c1d6dba598815061b072
Tania <113130993+Tania66@users.noreply.github.com>
2023-11-24 10:59:35 +0200

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import Profile from '../Profile/Profile';
-import user from '../user.json';
-import Statistics from 'Statistics/Statistics';
-import data from '../data.json';
-import FriendList from '../Friend/FriendList';
-import friends from '../friends.json';
-import TransactionHistory from '../Transaction/TransactionHistory';
-import transactions from '../transactions.json';
+import Profile from './Profile/Profile';
+import user from '../data/user.json';
+import Statistics from './Statistics/Statistics';
+import data from '../data/data.json';
+import FriendList from './Friend/FriendList';
+import friends from '../data/friends.json';
+import TransactionHistory from './Transaction/TransactionHistory';
+import transactions from '../data/transactions.json';
 
 export default function App() {
   return (
